e69d945d4bfd7d0e920b6163f1ae18663cdefa67
Abhirami <srideviabhirami.v@thoughtworks.com>
2022-03-25 11:10:11 +0530

    Add publisher repository

---
+++ src/main/java/com/example/springwebapp/repository/PublisherRepository.java
@@ -0,0 +1,9 @@
+package com.example.springwebapp.repository;
+
+import com.example.springwebapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
